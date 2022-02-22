3e0f406921e3b3b3b8d5e30e132140a268b5c2ab
Rudney V. G. Souza <rudney.vgs@gmail.com>
2022-02-22 12:52:18 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,11 +6,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
